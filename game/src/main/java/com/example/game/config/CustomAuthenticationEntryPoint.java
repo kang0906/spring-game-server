ea58a4d50e470db3b8bb2b1aff1f1fcd67105fb0
kang0906 <jinkoo906@gmail.com>
2024-04-29 16:18:47 +0900

    #8 잘못된 URL로 접근시 401 반환 -> 404 반환하도록 수정

--- game/src/main/java/com/example/game/config/CustomAuthenticationEntryPoint.java
+++ game/src/main/java/com/example/game/config/CustomAuthenticationEntryPoint.java
@@ -21,12 +21,15 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
+        if(response.getStatus() == HttpStatus.NOT_FOUND.value()){
+            return;
+        }
         ObjectMapper objectMapper = new ObjectMapper();
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         ErrorCode errorCode = ErrorCode.INVALID_USER;
         ErrorResponse errorResponse = new ErrorResponse(errorCode.getCode(), errorCode.getMessage());
         response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
-        log.warn("로그인을 다시 시도해 주세요");
+        log.warn("로그인 실패, http status : {} , exception message : {}", response.getStatus(), authException.getMessage());
     }
 }
